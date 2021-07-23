33b79670a1804e6ef115559d962b1d70e14ef36c
olgbat3313 <olgbat3313@gmail.com>
2021-07-23 13:46:47 +0300

    1_3_case_complete

--- homework3_1.py
+++ homework3_1.py
@@ -1,10 +1,16 @@
 #case1
-a = int(input('enter first number '))
-b = int(input('enter seconod number '))
-if 3<=a<=21 and 3<=b<=21:
-    print(abs(a-b))
+a = input('enter first number ')
+b = input('enter seconod number ')
+if a.isdigit() == True and b.isdigit() == True:
+    a = int(a)
+    b = int(b)
+    if 3<=a<=21 and 3<=b<=21:
+        print(abs(a-b))  
+    else:
+        print(a+b)
 else:
     print(a+b)
+   
 
 
        
