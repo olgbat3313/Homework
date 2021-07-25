0071b27dc6687f1dfd5577f2af519b7e3b2a29fb
olgbat3313 <87665227+olgbat3313@users.noreply.github.com>
2021-07-25 21:11:16 +0300

    Update homework3_5.py

--- homework3_5.py
+++ homework3_5.py
@@ -1,6 +1,17 @@
 a = input('enter number from 001 to 999 ')
 y = 0
-for x in a:
-    x = int(x)
-    y = y+x
-print(y)     
+b = len(a)
+if b == 3 and a.isdigit()==True:
+    for x in a:
+        x = int(x)
+        y = y+x
+    print(y)     
+else:
+    print('your number is wrong ') 
+if a.isdigit()==True:
+    for x in a:
+        x = int(x)
+        y = y+x
+    print(y)       
+else:
+    print('your number is wrong')    
